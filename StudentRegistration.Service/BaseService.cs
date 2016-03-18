c5922de3670dfd7598075989227a506ae0389756
linckon <linckon@live.com>
2016-03-18 15:34:06 +0600

    added student service

---
+++ StudentRegistration.Service/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentRegistration.Model;
+
+namespace StudentRegistration.Service
+{
+    public class BaseService
+    {
+        protected BusinessDbContext DbContext;
+
+        public BaseService(BusinessDbContext db)
+        {
+            DbContext = db;
+        }
+    }
+}
---
+++ StudentRegistration.Service/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentRegistration.Model;
+using StudentRegistration.Repository;
+using StudentRegistration.ViewModel;
+
+namespace StudentRegistration.Service
+{
+    public class StudentService:BaseService
+    {
+        private StudentRepository repository;
+        public StudentService(BusinessDbContext db) : base(db)
+        {
+             repository = new StudentRepository(DbContext);
+        }
+
+        public List<StudentViewModel> GetAll()
+        {
+            IQueryable<Student> queryable = repository.GetAll();
+            List<StudentViewModel> list = queryable.Select(x => new StudentViewModel(x)).ToList();
+            return list;
+        }
+
+        public string Add(Student student)
+        {
+            return repository.Add(student);
+        }
+    }
+}
