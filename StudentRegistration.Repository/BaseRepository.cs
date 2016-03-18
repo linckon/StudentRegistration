13ddf246d0582b26a58d4cbbb9d738ebeb65e88a
linckon <linckon@live.com>
2016-03-18 15:18:52 +0600

    student repository added

---
+++ StudentRegistration.Repository/BaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentRegistration.Model;
+
+namespace StudentRegistration.Repository
+{
+    public abstract class BaseRepository
+    {
+        public BusinessDbContext Db { get; set; }
+
+        protected BaseRepository(BusinessDbContext db)
+        {
+            this.Db = db;
+        }
+    }
+}
